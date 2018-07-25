d3f4b1c206d58faa7edc5338eaaa078552630db7
Alison Appling <aappling@usgs.gov>
2018-07-25 12:56:37 -0400

    debug 1_wqdata.yml + 1_wqp_data_harmonize_explorer.Rmd

--- lib/src/render_rmd.R
+++ lib/src/render_rmd.R
@@ -4,5 +4,8 @@
 #' @param output_file name of the desired file (probably .html)
 #' @param ... other arguments passed to rmarkdown::render
 render_rmd <- function(input, output_file, ...) {
-  rmarkdown::render(input=input, output_file=output_file, ...)
+  rmarkdown::render(input=input, output_file=basename(output_file), ...)
+  rmd_out <- file.path(dirname(input), basename(output_file)) 
+  file.copy(rmd_out, output_file)
+  file.remove(rmd_out)
 }
